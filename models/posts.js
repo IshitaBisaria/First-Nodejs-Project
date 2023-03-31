0393bf357be19d1b631472e7f7d0d7f8bb354569
Ishita Bisaria <81461876+IshitaBisaria@users.noreply.github.com>
2023-03-31 22:11:35 +0530

    Initial Commit

---
+++ models/posts.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const { v4: uuid } = require("uuid");
+uuid();
+
+const postsSchema = new mongoose.Schema({
+  p_id: {
+    type: String,
+    required: true,
+  },
+  // p_id: {
+  //   type: String,
+  //   default: function genUUID() {
+  //     uuid.v1();
+  //   },
+  // },
+  p_detail: {
+    type: String,
+    required: true,
+  },
+  mentor: {
+    type: String,
+    required: true,
+  },
+  mentor_username: {
+    type: String,
+    required: true,
+  },
+});
+
+const Post = mongoose.model("Post", postsSchema);
+
+module.exports = Post;
